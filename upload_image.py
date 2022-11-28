5b207d12f87e4b7ed6fd9f7ec06bded5cf25df85
ydoo123 <ydoo123@gmail.com>
2022-11-28 18:11:32 +0900

    Feat: Add check_dir() to create and find image_dir

--- upload_image.py
+++ upload_image.py
@@ -1,25 +1,47 @@
 import requests
+import os
 import json
 
 
+URL = "http://140.238.28.123/fileUpload"
+
+
+def check_dir():
+    """
+    이미지 파일 다루기 전, images 폴더가 있는지 확인. 없으면 images 폴더 생성
+    """
+    cwd = os.getcwd()
+    ls = os.listdir(cwd)
+
+    if "images" not in ls:
+        os.makedirs(f"{cwd}/images")
+
+    return None
+
+
 def upload_image(file_name, file_path):
+    """
+    URL로 이미지파일을 업로드하는 함수
+    """
     filename = file_name
     filepath = file_path
 
-    url = "http://140.238.28.123/fileUpload"
-
-    files = {
+    image_file = {
         "file": (filename, open(filepath, "rb")),
         "Content-Type": "image/jpg",
         # "Content-Length": l,
     }
-    requests.post(url, files=files)
+    requests.post(URL, files=image_file)
+
     return None
 
 
 if __name__ == "__main__":
-    
-    filename = "jpeg420exif.jpg"
-    filepath = f"/home/ubuntu/test_server/rudolph_bot/test/image/{filename}"
-    
-    result = upload_image(filename, filepath)
+
+    # filename = "jpeg420exif.jpg"
+    # filepath = f"/home/ubuntu/test_server/rudolph_bot/test/image/{filename}"
+
+    # result = upload_image(filename, filepath)
+
+    # check_dir()
+    print("test")
