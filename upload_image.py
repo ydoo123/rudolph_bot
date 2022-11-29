92a8af55aff8f43595f8f6574013866492d2b050
ydoo123 <ydoo123@gmail.com>
2022-11-29 14:15:10 +0900

    Feat: Save the cam screen to the current time.jpg

--- upload_image.py
+++ upload_image.py
@@ -1,9 +1,11 @@
 import requests
 import os
-import json
+import cv2
+import datetime
 
 
 URL = "http://140.238.28.123/fileUpload"
+TIME_FORMAT = "%Y-%m-%d_%H:%M:%S"
 
 
 def check_dir():
@@ -17,6 +19,30 @@ def check_dir():
         os.makedirs(f"{cwd}/images")
 
     return None
+
+
+def save_image():
+    """
+    웹캠 화면을 저장하는 코드.
+    return: 저장된 이미지 파일 이름(현재 시간), 저장된 이미지 파일 경로
+    """
+
+    now_time = datetime.datetime.now().strftime(TIME_FORMAT)
+
+    check_dir()
+    cam = cv2.VideoCapture(0)
+    ret, frame = cam.read()
+
+    img_name = f"images/image_{now_time}.jpg"
+    cv2.imwrite(img_name, frame)
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    file_name = f"image_{now_time}.jpg"
+    file_path = f"images/{file_name}"
+
+    return file_name, file_path
 
 
 def upload_image(file_name, file_path):
@@ -38,10 +64,5 @@ def upload_image(file_name, file_path):
 
 if __name__ == "__main__":
 
-    # filename = "jpeg420exif.jpg"
-    # filepath = f"/home/ubuntu/test_server/rudolph_bot/test/image/{filename}"
-
-    # result = upload_image(filename, filepath)
-
-    # check_dir()
-    print("test")
+    file_name, file_path = save_image()
+    upload_image(file_name, file_path)
