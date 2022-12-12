83be0b8c9223d559a07a788ba2a12826320a2b50
ydoo123 <ydoo123@gmail.com>
2022-12-12 14:38:02 +0000

    Fix: add create db

--- create_db.py
+++ create_db.py
@@ -2,12 +2,15 @@ import sqlite3
 
 
 conn = sqlite3.connect("database.db")
-print("create db success")
+print("success")
 
 conn.execute(
     """
-    create table images (image_name text, image_dir text, time SMALLDATETIME);
+    create table dests (name text, phone_number text, dest text, method text, time SMALLDATETIME)
     """
+)
+conn.execute(
+    """create table images (image_name text, image_dir text, time SMALLDATETIME);"""
 )
 print("create table success")
 
